39cb630d300721035f4dcdcb493ce3a799f69c2c
John Young <john.young@foxmail.com>
2026-06-29 22:26:22 +0800

    feat: update

--- portal/server/internal/pkg/sms/sms_test.go
+++
@@ -1,8 +0,0 @@
-package sms
-
-import "testing"
-
-func TestSms(t *testing.T) {
-	err := SendSMS([]string{"+8618512869479"}, "2581380", []string{"3306"}, "明河", "1401074343", "yourSecretId", "yourSecretKey")
-	t.Log(err)
-}
